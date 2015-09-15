17d5199ce6ef39a41367aef0243f08510d165644
MRijlaar <mrijlaar@ualberta.ca>
2015-09-14 19:46:59 -0600

    Pressed undo till befoe I wecked stuff trying to add generic types

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
@@ -1,14 +1,13 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * Created by mrijlaar on 9/14/15.
  */
 public abstract class Mood {
+    private String mood;
     private Date date;
-    private ArrayList<Mood> mood;
 
     public Mood(String mood) {
         this.mood = mood;
